36a93737eb02322fd2bb01c98db25c4725923542
unknown <khanhnqps10920@fpt.edu.vn>
2019-07-06 19:41:07 +0700

    test build

---
+++ build/precache-manifest.d6d385a823a8be7815acf92b2a5b589c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88b74406ad957bc4465e9b0e391ffb0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "273ea9058bba756ea0e8",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c0a022932895bff65037",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "273ea9058bba756ea0e8",
+    "url": "./static/js/2.acaed131.chunk.js"
+  },
+  {
+    "revision": "c0a022932895bff65037",
+    "url": "./static/js/main.c459ae8e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
